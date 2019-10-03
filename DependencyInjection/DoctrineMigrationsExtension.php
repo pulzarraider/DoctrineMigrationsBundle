7a1f18ef1ae8419da4493948f70617f02ba32576
Asmir Mustafic <goetas@gmail.com>
2019-10-03 13:52:24 +0200

    Adapt bundle for migrations 3.0

--- DependencyInjection/DoctrineMigrationsExtension.php
+++ DependencyInjection/DoctrineMigrationsExtension.php
@@ -4,10 +4,19 @@ declare(strict_types=1);
 
 namespace Doctrine\Bundle\MigrationsBundle\DependencyInjection;
 
+use Doctrine\DBAL\Connection;
+use Doctrine\Migrations\DependencyFactory;
+use Doctrine\Migrations\Metadata\Storage\MetadataStorage;
+use Doctrine\Migrations\Metadata\Storage\TableMetadataStorageConfiguration;
+use InvalidArgumentException;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use function sprintf;
 
 /**
  * DoctrineMigrationsExtension.
@@ -25,14 +34,92 @@ class DoctrineMigrationsExtension extends Extension
 
         $config = $this->processConfiguration($configuration, $configs);
 
-        foreach ($config as $key => $value) {
-            $container->setParameter($this->getAlias() . '.' . $key, $value);
-        }
-
         $locator = new FileLocator(__DIR__ . '/../Resources/config/');
         $loader  = new XmlFileLoader($container, $locator);
 
         $loader->load('services.xml');
+
+        $configurationDefinition = $container->getDefinition('doctrine.migrations.configuration');
+
+        $configurationDefinition->addMethodCall('setName', [$config['name']]);
+
+        foreach ($config['migrations_paths'] as $ns => $path) {
+            $configurationDefinition->addMethodCall('addMigrationsDirectory', [$ns, $path]);
+        }
+
+        foreach ($config['migrations'] as $migrationClass) {
+            $configurationDefinition->addMethodCall('addMigrationClass', [$migrationClass]);
+        }
+
+        if ($config['organize_migrations'] !== false) {
+            $configurationDefinition->addMethodCall('setMigrationOrganization', [$config['organize_migrations']]);
+        }
+
+        if ($config['custom_template'] !== null) {
+            $configurationDefinition->addMethodCall('setCustomTemplate', [$config['custom_template']]);
+        }
+
+        $configurationDefinition->addMethodCall('setAllOrNothing', [$config['all_or_nothing']]);
+        $configurationDefinition->addMethodCall('setCheckDatabasePlatform', [$config['check_database_platform']]);
+
+        $diDefinition = $container->getDefinition('doctrine.migrations.dependency_factory');
+
+        if ($config['sorter'] !== null) {
+            $diDefinition->addMethodCall('setService', [DependencyFactory::MIGRATIONS_SORTER, new Reference($config['sorter'])]);
+        }
+
+        if ($config['storage']['id'] !== null) {
+            $diDefinition->addMethodCall('setService', [MetadataStorage::class, new Reference($config['storage']['id'])]);
+        } else {
+            $storageConfiguration = $config['storage']['table_storage'];
+
+            $storageDefinition = new Definition(TableMetadataStorageConfiguration::class);
+            $container->setDefinition('doctrine.migrations.storage.table_storage', $storageDefinition);
+            $container->setAlias('doctrine.migrations.metadata_storage', 'doctrine.migrations.storage.table_storage');
+
+            if ($storageConfiguration['table_name']!== null) {
+                $storageDefinition->addMethodCall('setTableName', [$storageConfiguration['table_name']]);
+            }
+            if ($storageConfiguration['version_column_name']!== null) {
+                $storageDefinition->addMethodCall('setVersionColumnName', [$storageConfiguration['version_column_name']]);
+            }
+            if ($storageConfiguration['version_column_length']!== null) {
+                $storageDefinition->addMethodCall('setVersionColumnLength', [$storageConfiguration['version_column_length']]);
+            }
+            if ($storageConfiguration['executed_at_column_name']!== null) {
+                $storageDefinition->addMethodCall('setExecutedAtColumnName', [$storageConfiguration['executed_at_column_name']]);
+            }
+            if ($storageConfiguration['execution_time_column_name']!== null) {
+                $storageDefinition->addMethodCall('setExecutionTimeColumnName', [$storageConfiguration['execution_time_column_name']]);
+            }
+
+            $configurationDefinition->addMethodCall('setMetadataStorageConfiguration', [new Reference('doctrine.migrations.storage.table_storage')]);
+        }
+
+        if ($config['em'] !== null && $config['connection'] !== null) {
+            throw new InvalidArgumentException('You can not specify both "connection" and "em" in the DoctrineMigrationsBundle configurations.');
+        }
+
+        $emID = $config['em'] !== null ? sprintf('doctrine.orm.%s_entity_manager', $config['em']) : null;
+
+        if ($emID !== null) {
+            $connectionDef = new Definition(Connection::class);
+            $connectionDef->setFactory([new Reference($emID), 'getConnection']);
+
+            $connectionId = sprintf('doctrine.migrations.connection.%s', $emID);
+            $container->setDefinition($connectionId, $connectionDef);
+
+            $em         = new Reference($emID);
+            $connection = new Reference($connectionId);
+        } else {
+            $connectionId = sprintf('doctrine.dbal.%s_connection', $config['connection'] ?? 'default');
+            $connection   = new Reference($connectionId);
+            $em           = null;
+        }
+
+        $diDefinition->setArgument(1, $connection);
+        $diDefinition->setArgument(2, $em);
+        $diDefinition->setArgument(3, new Reference('logger', ContainerInterface::NULL_ON_INVALID_REFERENCE));
     }
 
     /**
--- Tests/DependencyInjection/DoctrineMigrationsExtensionTest.php
+++ Tests/DependencyInjection/DoctrineMigrationsExtensionTest.php
@@ -5,27 +5,205 @@ declare(strict_types=1);
 namespace Doctrine\Bundle\MigrationsBundle\Tests\DependencyInjection;
 
 use Doctrine\Bundle\MigrationsBundle\DependencyInjection\DoctrineMigrationsExtension;
+use Doctrine\Bundle\MigrationsBundle\Tests\Fixtures\CustomEntityManager;
+use Doctrine\DBAL\Connection;
 use Doctrine\Migrations\Configuration\Configuration;
+use Doctrine\Migrations\DependencyFactory;
+use Doctrine\Migrations\Metadata\Storage\MetadataStorage;
+use Doctrine\Migrations\Metadata\Storage\TableMetadataStorageConfiguration;
+use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
+use function assert;
+use function method_exists;
 use function sys_get_temp_dir;
 
 class DoctrineMigrationsExtensionTest extends TestCase
 {
-    public function testOrganizeMigrations() : void
+    public function testFullConfig() : void
     {
         $container = $this->getContainer();
         $extension = new DoctrineMigrationsExtension();
 
-        $config = ['organize_migrations' => 'BY_YEAR'];
+        $config = [
+            'name' => 'Doctrine Sandbox Migrations',
+            'storage' => [
+                'table_storage' => [
+                    'table_name'                 => 'doctrine_migration_versions_test',
+                    'version_column_name'        => 'doctrine_migration_column_test',
+                    'version_column_length'      => 2000,
+                    'executed_at_column_name'    => 'doctrine_migration_executed_at_column_test',
+                    'execution_time_column_name' => 'doctrine_migration_execution_time_column_test',
+                ],
+            ],
+
+            'migrations_paths' => [
+                'DoctrineMigrationsTest' => 'a',
+                'DoctrineMigrationsTest2' => 'b',
+            ],
+
+            'migrations' => ['Foo', 'Bar'],
+
+            'organize_migrations' => 'BY_YEAR_AND_MONTH',
+
+            'all_or_nothing'            => true,
+            'check_database_platform'   => true,
+        ];
+
+        $conn = $this->createMock(Connection::class);
+        $container->set('doctrine.dbal.default_connection', $conn);
 
         $extension->load(['doctrine_migrations' => $config], $container);
 
-        $this->assertEquals(
-            Configuration::VERSIONS_ORGANIZATION_BY_YEAR,
-            $container->getParameter('doctrine_migrations.organize_migrations')
-        );
+        $container->getDefinition('doctrine.migrations.configuration')->setPublic(true);
+        $container->compile();
+
+        $config = $container->get('doctrine.migrations.configuration');
+
+        self::assertInstanceOf(Configuration::class, $config);
+        self::assertSame('Doctrine Sandbox Migrations', $config->getName());
+        self::assertSame([
+            'DoctrineMigrationsTest' => 'a',
+            'DoctrineMigrationsTest2' => 'b',
+
+        ], $config->getMigrationDirectories());
+
+        self::assertSame(['Foo', 'Bar'], $config->getMigrationClasses());
+        self::assertTrue($config->isAllOrNothing());
+        self::assertTrue($config->isDatabasePlatformChecked());
+        self::assertTrue($config->areMigrationsOrganizedByYearAndMonth());
+
+        $storage = $config->getMetadataStorageConfiguration();
+        self::assertInstanceOf(TableMetadataStorageConfiguration::class, $storage);
+
+        self::assertSame('doctrine_migration_versions_test', $storage->getTableName());
+        self::assertSame('doctrine_migration_column_test', $storage->getVersionColumnName());
+        self::assertSame(2000, $storage->getVersionColumnLength());
+        self::assertSame('doctrine_migration_execution_time_column_test', $storage->getExecutionTimeColumnName());
+        self::assertSame('doctrine_migration_executed_at_column_test', $storage->getExecutedAtColumnName());
+    }
+
+    public function testCustomSorter() : void
+    {
+        $container = $this->getContainer();
+        $extension = new DoctrineMigrationsExtension();
+
+        $config = ['sorter' => 'my_sorter'];
+
+        $extension->load(['doctrine_migrations' => $config], $container);
+
+        $container->getDefinition('doctrine.migrations.dependency_factory')->setPublic(true);
+
+        $conn = $this->createMock(Connection::class);
+        $container->set('doctrine.dbal.default_connection', $conn);
+
+        $sorter = new class(){
+            public function __invoke() : void
+            {
+            }
+        };
+        $container->set('my_sorter', $sorter);
+
+        $container->compile();
+
+        $di = $container->get('doctrine.migrations.dependency_factory');
+        self::assertInstanceOf(DependencyFactory::class, $di);
+        self::assertSame($sorter, $di->getSorter());
+    }
+
+    public function testCustomConnection() : void
+    {
+        $container = $this->getContainer();
+        $extension = new DoctrineMigrationsExtension();
+
+        $config = ['connection' => 'custom'];
+
+        $extension->load(['doctrine_migrations' => $config], $container);
+
+        $container->getDefinition('doctrine.migrations.dependency_factory')->setPublic(true);
+
+        $conn = $this->createMock(Connection::class);
+        $container->set('doctrine.dbal.custom_connection', $conn);
+
+        $container->compile();
+
+        $di = $container->get('doctrine.migrations.dependency_factory');
+        self::assertInstanceOf(DependencyFactory::class, $di);
+        self::assertSame($conn, $di->getConnection());
+    }
+
+    public function testCustomEntityManager() : void
+    {
+        $container = $this->getContainer();
+        $extension = new DoctrineMigrationsExtension();
+
+        $config = ['em' => 'custom'];
+
+        $em = new Definition(CustomEntityManager::class);
+        $container->setDefinition('doctrine.orm.custom_entity_manager', $em);
+
+        $extension->load(['doctrine_migrations' => $config], $container);
+
+        $container->getDefinition('doctrine.migrations.dependency_factory')->setPublic(true);
+
+        $container->compile();
+
+        $di = $container->get('doctrine.migrations.dependency_factory');
+        self::assertInstanceOf(DependencyFactory::class, $di);
+
+        $em = $di->getEntityManager();
+        self::assertInstanceOf(CustomEntityManager::class, $em);
+
+        assert(method_exists($di->getConnection(), 'getEm'));
+        self::assertInstanceOf(CustomEntityManager::class, $di->getConnection()->getEm());
+        self::assertSame($em, $di->getConnection()->getEm());
+    }
+
+    public function testCustomMetadataStorage() : void
+    {
+        $container = $this->getContainer();
+        $extension = new DoctrineMigrationsExtension();
+
+        $config = [
+            'storage' => ['id' => 'mock_storage_service'],
+        ];
+
+        $mockStorage = $this->createMock(MetadataStorage::class);
+        $container->set('mock_storage_service', $mockStorage);
+
+        $conn = $this->createMock(Connection::class);
+        $container->set('doctrine.dbal.default_connection', $conn);
+
+        $extension->load(['doctrine_migrations' => $config], $container);
+
+        $container->getDefinition('doctrine.migrations.dependency_factory')->setPublic(true);
+
+        $container->compile();
+
+        $di = $container->get('doctrine.migrations.dependency_factory');
+        self::assertInstanceOf(DependencyFactory::class, $di);
+        self::assertSame($mockStorage, $di->getMetadataStorage());
+    }
+
+    public function testCanNotSpecifyBothEmAndConnection() : void
+    {
+        $this->expectExceptionMessage('You can not specify both "connection" and "em" in the DoctrineMigrationsBundle configurations');
+        $this->expectException(InvalidArgumentException::class);
+        $container = $this->getContainer();
+        $extension = new DoctrineMigrationsExtension();
+
+        $config = [
+            'em' => 'custom',
+            'connection' => 'custom',
+        ];
+
+        $extension->load(['doctrine_migrations' => $config], $container);
+
+        $container->getDefinition('doctrine.migrations.dependency_factory')->setPublic(true);
+
+        $container->compile();
     }
 
     private function getContainer() : ContainerBuilder
